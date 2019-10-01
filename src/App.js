8d24e208c512fb20042497acb088ff4acc013ecf
eTargt1 <louisclifford10@yahoo.fr>
2019-09-30 20:30:30 -0400

    using arrow function in es6 followed vd 5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet';
+import MyComponent from './components/Greet';
 
 function App() {
   return (
     <div className="App">
-      <Greet />
+      <MyComponent />
     </div>
   );
 } 
