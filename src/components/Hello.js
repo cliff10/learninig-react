1ffd234eb0c0bc1f9c9b3750d53f81a9411f16fc
eTargt1 <louisclifford10@yahoo.fr>
2019-09-30 21:22:32 -0400

    react compoment without using jsx

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,8 +1,11 @@
 import React from 'react';
 const Hello = () => {
-    return (
-        <div>Hello Everyone!</div>
-    )
+//     return (
+//         <div>Hello Everyone!</div>
+//     )
+
+return React.createElement('div', null, React.createElement('h1', null, 'Hi everyone!' ))
+
 }
 
 export default Hello;
