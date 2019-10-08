05fc32c43031b455d28fa045e2e833a8d2c756b8
eTargt1 <louisclifford10@yahoo.fr>
2019-10-08 00:59:03 -0400

    Used state to update the Message react component

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import MyComponent from './components/Greet';
 import Welcome from './components/Welcome'
 import Hello from './components/Hello';
 import Greet from './components/Greet';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      <Greet firstName="Clifford" lastName="Louis">
+      {/* <Greet firstName="Clifford" lastName="Louis">
         <p> This is the basic of your personal info. </p>
       </Greet>
       <Greet firstName="Jefferson" lastName="Pierre">
@@ -21,7 +22,8 @@ function App() {
       
       <Welcome firstName="Clifford" lastName="Louis" />
       <Welcome  firstName="Jefferson" lastName="Pierre" />
-      <Welcome firstName="Jefferson" lastName="Pierre" />
+      <Welcome firstName="Jefferson" lastName="Pierre" /> */}
+      <Message />
     </div>
   );
 } 
